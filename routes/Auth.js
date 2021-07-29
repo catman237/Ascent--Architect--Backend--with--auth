948c4e850e05e20585310087211edc60afa61a55
Greg <Ghughes610@gmail.com>
2021-07-29 11:33:16 -0600

    auth is done

---
+++ routes/Auth.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const { JsonWebTokenError } = require('jsonwebtoken')
+const router = express.Router()
+const jwt = require('jsonwebtoken')
+const { User } = require('../models/User')
+
+
+const authenticate = (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        res.send(403).send({ error: 'no bearer token present' })
+    } else {
+        const token = authorization.split(' ')[1]
+        const secret = process.env.AUTH_SECRET
+        try {
+            const decodedToken = jwt.verify(token, secret)
+            const { user_id } = decodedToken
+            User.query()
+                .findById(user_id)
+                .then(existingUser => {
+                    req.user = existingUser
+                    next()
+                })
+        } catch (error) {
+            res.status(403).send({ error: 'invalid token' })
+        }
+    }
+}
+
+module.exports = { authenticate }
--- routes/Users.js
+++ routes/Users.js
@@ -6,6 +6,7 @@ const config = require('../knexfile')[process.env.NODE_ENV || 'development']
 const knex = require('knex')(config);
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const { authenticate } = require('./Auth')
 
 router.get('/users', (_, res) => {
     User.query()
@@ -13,45 +14,47 @@ router.get('/users', (_, res) => {
         .then(users => res.status(200).json(users))
 })
 
-router.get('/users/:id', (req, res) => {
-    const id = req.params.id
-    User.query()
-    .where('id', id)
-    .first()
-    .then(user => res.status(200).json(user))
-})
+
+//protect this
+router.get('/users/:id', authenticate, (req, res) => {
+        const id = req.params.id
+        User.query()
+            .where('id', id)
+            .first()
+            .then(user => res.status(200).json(user))
+    })
 
 router.delete('/users/:id', (req, res) => {
-   User.query()
-   .where('id', req.params.id)
-   .withGraphFetched('climbs')
-   .delete()
-   .then(deletedUser => res.status(200).json(deletedUser))
+    User.query()
+        .where('id', req.params.id)
+        .withGraphFetched('climbs')
+        .delete()
+        .then(deletedUser => res.status(200).json(deletedUser))
 })
 
 
-router.post('/login', (req, res) => {
+router.post('/login', (req, res, next) => {
     const { user } = req.body
 
     User.query()
-    .findOne({ username: user.username || '' })
-    .then(existingUser => {
-        if (!existingUser){
-            res.status(401).json({ message: 'Invalid username of password' })
-        } else {
-            bcrypt.compare(user.password, existingUser.password_digest)
-            .then(isMatch => {
-                if (!isMatch) {
-                    res.status(401).json({ message: 'Invalid username or password' })
-                } else {
-                    const secret = process.env.AUTH_SECRET
-                    const payload = { user_id: existingUser.id }
-                    const token = jwt.sign(payload, secret)
-                    res.status(200).json({ token })
-                }
-            })
-        }
-    })
+        .findOne({ username: user.username || '' })
+        .then(existingUser => {
+            if (!existingUser) {
+                res.status(401).json({ message: 'Invalid username of password' })
+            } else {
+                bcrypt.compare(user.password, existingUser.password_digest)
+                    .then(isMatch => {
+                        if (!isMatch) {
+                            res.status(401).json({ message: 'Invalid username or password' })
+                        } else {
+                            const secret = process.env.AUTH_SECRET
+                            const payload = { user_id: existingUser.id }
+                            const token = jwt.sign(payload, secret)
+                            res.status(200).json({ token })
+                        }
+                    })
+            }
+        })
 })
 
 
